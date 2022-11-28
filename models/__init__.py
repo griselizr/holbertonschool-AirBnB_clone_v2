265ad0dcee5473948d3e19e0adaf2f576e1a5cf4
Griseliz <git-commit-address>
2022-11-27 22:45:57 -0400

    insert conditional depending of the value of the environment variable HBNB_TYPE_STORAGE

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from os import getenv
+from models.engine.db_storage import DBStorage
 
+enviroment = getenv('HBNB_TYPE_STORAGE')
 
-storage = FileStorage()
-storage.reload()
+if enviroment == 'db':
+    storage = DBStorage()
+    storage.reload()
+
+else:
+    storage = FileStorage()
+    storage.reload()
