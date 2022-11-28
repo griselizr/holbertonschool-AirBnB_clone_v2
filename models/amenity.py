b4ef5a77cfe1091dc324ac8afe6924225f36891f
Griseliz <git-commit-address>
2022-11-27 23:03:38 -0400

    User inherits from BaseModel and Base

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            'Place',
+            secondary='place_amenity',
+            cascade='delete'
+        )
     name = ""
