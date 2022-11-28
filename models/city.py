1ac5c84914606be5b2a068ba6980075aaa31e4c8
Griseliz <git-commit-address>
2022-11-27 22:42:36 -0400

    inherits from BaseModel and Base

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""" State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class State(BaseModel):
+    """ State class """
+    """ The city class with state id nd name """
+    __tablename__ = 'cities'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+        state_id = ""
