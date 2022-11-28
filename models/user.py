7f7df71bf83652bdee7a9a9f34d9848262b446d2
Griseliz <git-commit-address>
2022-11-27 22:56:56 -0400

    User inherits from BaseModel and Base

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -7,7 +7,7 @@ from os import getenv
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """Defines a user by various attributes"""
     __tablename__ = 'users'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
