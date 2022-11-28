52167f46a8fd6f9b56664e026d5c1fdab5b6cc81
Griseliz <git-commit-address>
2022-11-27 23:12:15 -0400

    User inherits from BaseModel and Base

--- models/place.py
+++ models/place.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from os import getenv
+from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('amenity_id', String(60), ForeignKey(
+                          'amenities.id'), nullable=False, primary_key=True),
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), nullable=False, primary_key=True)
+                      )
+
+
+class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = 'places'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', cascade='delete')
+        amenities = relationship(
+            'Amenity',
+            secondary=place_amenity,
+            viewonly=False
+        )
+
+    else:
     city_id = ""
     user_id = ""
     name = ""
