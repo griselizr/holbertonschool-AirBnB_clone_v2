59992c07d3451401b8bbc34c5ea137839751c1f7
Griseliz <git-commit-address>
2022-11-28 22:29:31 -0400

    set value text to display

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+"""start the Flask Web application by"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """return a string from route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """return a string from hbnb route"""
+    return 'HBNB'
+
+
+app.route('//c/<text>')
+
+
+def display_c(text):
+    """return a string from c /text"""
+    return "C " + text.replace("_", "")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
