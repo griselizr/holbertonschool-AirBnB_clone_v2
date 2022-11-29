71b6db1e67e3866a9db0a3f80132b15fe55c6046
Griseliz <git-commit-address>
2022-11-28 22:19:22 -0400

    web application with another display hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""start the Flask Web application by"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """return a string from route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """return a string from hbnb route"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
