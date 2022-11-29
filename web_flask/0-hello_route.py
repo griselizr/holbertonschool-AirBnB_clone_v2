7e79c8fdbddd2958de6836c7b2b9d8d0fd32bd26
Griseliz <git-commit-address>
2022-11-28 21:16:17 -0400

    script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+""" setting strict slashes to false to configure the app itself"""
+"""return a string from route"""
+
+
+@app.route("/", strict_slashes=False)
+def hello_holberton():
+    return "<p>“Hello HBNB!”!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
