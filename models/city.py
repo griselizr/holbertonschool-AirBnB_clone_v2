d8106ce5d6e9b4050b31f5862e503e27bdc2e567
Griseliz <git-commit-address>
2022-11-30 15:36:54 -0400

    remove Base

--- models/city.py
+++ models/city.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
