621fe1da03c01140fcc8711155279217676995f4
Griseliz <git-commit-address>
2022-11-29 22:14:14 -0400

    insert root for python text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+"""start the Flask Web application by"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """return a string from route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return a string from hbnb route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """return a string from c /text"""
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    """return a string from python/text"""
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
