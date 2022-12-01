d6fde61b58deb36a6a1e7c87044e1239b0cfb4b2
Griseliz <git-commit-address>
2022-12-01 13:57:23 -0400

    include Base inheritage

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
