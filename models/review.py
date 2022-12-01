e3e345a0f6c81bc031b86cb6f45fd19a267769d7
Griseliz <git-commit-address>
2022-12-01 13:28:02 -0400

    add inheritage Base

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
