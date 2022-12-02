119af2fb308cca48641fa748e99a86ae55cd77e3
Griseliz <git-commit-address>
2022-12-01 21:46:46 -0400

    add again

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel,
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
 
@@ -8,12 +8,12 @@ from os import getenv
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    getenv('HBNB_TYPE_STORAGE') == 'db'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    place_id = ""
-    user_id = ""
-    text = ""
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
