4e23d40e77c1ecbbd91ba60123fc797450eb3367
Griseliz <git-commit-address>
2022-12-01 20:26:16 -0400

    remove import

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,6 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from os import getenv
-from models.engine.db_storage import DBStorage
 
 enviroment = getenv('HBNB_TYPE_STORAGE')
 
