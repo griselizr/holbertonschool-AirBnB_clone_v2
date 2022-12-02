18d4df98afa70b524a724a6faa7bbf9febd79588
Griseliz <git-commit-address>
2022-12-01 21:42:59 -0400

    remove conditions

--- models/review.py
+++ models/review.py
@@ -8,12 +8,12 @@ from os import getenv
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    getenv('HBNB_TYPE_STORAGE') == 'db':
 
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    place_id = ""
+    user_id = ""
+    text = ""
