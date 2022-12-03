a0c9bacb340fe7d8a7ea4c5e4e9f536365863032
Griseliz <git-commit-address>
2022-12-03 18:44:25 -0400

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+
+
 from os import getenv
 
 enviroment = getenv('HBNB_TYPE_STORAGE')
 
-if enviroment == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
+
 
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
