d38a2d3b82250372ca0485a037d4a1c77addbfff
Griseliz <git-commit-address>
2022-12-03 19:57:22 -0400

    add loop for storage

--- models/city.py
+++ models/city.py
@@ -3,12 +3,16 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ The city class attributes  contains state ID and name plus places """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     state = relationship('State')
     places = relationship('Place', cascade="delete")
+    in_storage = os.environ.get('HBNB_TYPE_STORAGE')
+    if in_storage == 'db':
+        state = relationship("State", backref="cities")
